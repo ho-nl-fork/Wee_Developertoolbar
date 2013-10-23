104cad42f2d7caaf083a583ab9cc6405b11d65f7
Paul Hachmang <paul@h-o.nl>
2013-10-24 01:34:10 +0200

    [~BUGFIX] Better compability with Aoe_Profiler

--- htdocs/app/code/community/Wee/DeveloperToolbar/Model/Observer.php
+++ htdocs/app/code/community/Wee/DeveloperToolbar/Model/Observer.php
@@ -4,6 +4,8 @@ class Wee_DeveloperToolbar_Model_Observer
 {
     public function removeCoreProfilerBlock($observer)
     {
-        Mage::app()->getLayout()->removeOutputBlock('core_profiler');
+        if (Mage::helper('core')->isModuleEnabled('Aoe_Profiler')) {
+            Mage::app()->getLayout()->removeOutputBlock('core_profiler');
+        }
     }
 }
