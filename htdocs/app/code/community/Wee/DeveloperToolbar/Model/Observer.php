4c377e67cb21163ad6f10b9d154c483db93219e8
Paul Hachmang <paul@h-o.nl>
2014-08-20 12:19:35 +0200

    [Fix] Make sure the developer toolbar resources aren't loaded when the module is disabled.

--- htdocs/app/code/community/Wee/DeveloperToolbar/Model/Observer.php
+++ htdocs/app/code/community/Wee/DeveloperToolbar/Model/Observer.php
@@ -8,4 +8,14 @@ class Wee_DeveloperToolbar_Model_Observer
             Mage::app()->getLayout()->removeOutputBlock('core_profiler');
         }
     }
+
+    public function addWeeDeveloperToolbarHandle(Varien_Event_Observer $observer) {
+        if (Mage::getStoreConfigFlag('advanced/modules_disable_output/Wee_DeveloperToolbar')) {
+            return;
+        }
+
+        /** @var Mage_Core_Model_Layout_Update $update */
+        $update = Mage::getSingleton('core/layout')->getUpdate();
+        $update->addHandle('wee_developertoolbar');
+    }
 }
